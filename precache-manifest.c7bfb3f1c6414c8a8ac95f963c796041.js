3cb38e24a8d49f9d62227bcd080bb74e888ae192
Leonardo Vitale <leonardo.vitale@outlook.com>
2019-03-28 01:20:42 +0000

    Updates

---
+++ precache-manifest.c7bfb3f1c6414c8a8ac95f963c796041.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7140ce38ad6c05c2e2ef",
+    "url": "/static/js/main.959a9220.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "20e2c6e8deb4cac3dfc3",
+    "url": "/static/js/2.a920164e.chunk.js"
+  },
+  {
+    "revision": "b96bddb7ecea235844ef17398dd2eb4f",
+    "url": "/index.html"
+  }
+];
